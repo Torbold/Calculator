3b76d69c0845f3cf3f39a8462ce219a60988952d
Mikhail Kareniuhin <mikhailkareniuhin@air-mikhail>
2016-03-29 23:04:34 +0300

    Calculator does the following operations:  - add  - subtract  - divide  - multiply  - clear

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,19 +7,136 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    enum Operation : String {
+        case Add = "+"
+        case Subtract = "-"
+        case Divide = "/"
+        case Multiply = "*"
+        case Equal = "="
+        case Empty = ""
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var runningNumber: String = ""
+    var leftValStr: String = ""
+    var rightValStr: String = ""
+    
+    var currentOperation: Operation = .Empty
+    
+    var btnSound: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        let soundURL = NSURL(fileURLWithPath: path!)
+       
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    @IBAction func numButtonPressed(sender: UIButton) {
+        playSound()
+        
+        runningNumber += "\(sender.tag)"
+        outputLbl.text = runningNumber
+        
+    }
+    
+    @IBAction func onDividePressed(sender: UIButton) {
+        processOperation(.Divide)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onMultiplyPressed(sender: UIButton) {
+        processOperation(.Multiply)
     }
-
-
+    
+    @IBAction func onSubtractPressed(sender: UIButton) {
+        processOperation(.Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: UIButton) {
+        processOperation(.Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: UIButton) {
+        processOperation(.Equal)
+    }
+    
+    @IBAction func onClearClick(sender: UIButton) {
+        if runningNumber == "" {
+            runningNumber = ""
+            leftValStr = ""
+            rightValStr = ""
+            outputLbl.text = "0.0"
+            currentOperation = .Empty
+        } else {
+            runningNumber = ""
+            outputLbl.text = "0.0"
+        }
+        
+    }
+    
+    func processOperation(op: Operation) {
+        playSound()
+        
+        if (currentOperation != .Empty) {
+            if (runningNumber == "" && currentOperation == op || runningNumber != "") {
+                rightValStr = runningNumber == "" ? leftValStr : runningNumber
+                switch currentOperation {
+                case .Add:
+                    leftValStr = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                    runningNumber = ""
+                    currentOperation = op
+                    outputLbl.text = leftValStr
+                    break
+                case .Divide:
+                    leftValStr = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                    runningNumber = ""
+                    currentOperation = op
+                    outputLbl.text = leftValStr
+                case .Multiply:
+                    leftValStr = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                    runningNumber = ""
+                    currentOperation = op
+                    outputLbl.text = leftValStr
+                case .Subtract:
+                    leftValStr = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                    runningNumber = ""
+                    currentOperation = op
+                    outputLbl.text = leftValStr
+                default:
+                    currentOperation = op
+                    outputLbl.text = leftValStr
+                    runningNumber = ""
+                }
+            } else {
+                currentOperation = op
+            }
+        } else {
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = op
+        }
+    }
+    
+    func playSound() {
+        if btnSound.playing {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
+    
 }
 
